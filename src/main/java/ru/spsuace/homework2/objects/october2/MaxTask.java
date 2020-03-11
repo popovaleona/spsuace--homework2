812a761a67a756dbf01097cce80f61cb48e67768
popovaleona <Popovaleona@gmail.com>
2020-03-11 22:19:13 +0300

    Homework2

--- src/main/java/ru/spsuace/homework2/objects/october2/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.spsuace.homework2.objects.october2;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -11,7 +13,16 @@ public class MaxTask {
      *
      */
     public static int[] getMaxArray(int[] array, int count) {
-        return null;
+        //проверка
+        if (array.length < count) {
+            return null;
+        }
+        //сортировка
+        Arrays.sort(array);
+        int[] Mas = new int[count];
+        for (int i = 0; i < count; i++) {
+            Mas[i] = array[array.length - i - 1];
+        }
+        return Mas;
     }
-
 }
